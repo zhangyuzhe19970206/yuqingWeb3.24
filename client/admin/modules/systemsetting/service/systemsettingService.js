c86637aebbfa28f90bbfb3bcd292da462b90dd75
zhangyuzhe19970206 <33304456+zhangyuzhe19970206@users.noreply.github.com>
2020-06-20 15:23:19 +0800

    8899

--- client/admin/modules/systemsetting/service/systemsettingService.js
+++ client/admin/modules/systemsetting/service/systemsettingService.js
@@ -4,7 +4,7 @@ angular.module('systemsettingService',['commons'])
     .factory('adminattrService', ['$http', function($http){
         var factories = {};
         factories.getUserattrData = function() {
-            return $http.get("http://39.102.48.39:8090/yqdata/user_attr");
+            return $http.get("http://39.102.48.39:8899/yqdata/user_attr");
         };
         return factories;
     }]);
